3f260858224af5005af83957f4979ebe1da58f60
Giulia <giuliaprivato98@gmail.com>
2021-04-02 22:34:08 +0200

    Add filtering by year

--- app.R
+++ app.R
@@ -44,12 +44,23 @@ ui<-fluidPage(
         label = 'Select',
         choices = data$country,
         multiple= TRUE),
+    helpText("2. Select year range"),
+    sliderInput(inputId = "yrrange",
+                label = "Select",
+                min = 2010,
+                max = 2019,
+                value = c(2010, 2011),
+                step=1,
+                sep=''),
     ),# END sideBarPanel
     
     mainPanel(
       column(12,
-             p('The graph below depicts average number of healthy years for individuals in all EU member states.
-              Select a country or a number of countries from the dropdown menu to the left.'),
+             p('The graph below depicts the average number of healthy years of individuals across EU member states.'),
+             p('Select a country or a number of countries from the dropdown menu to the left.
+              Use the slider to select the year-range of interest and therefore visualize time-trends in HLY.
+              Using the dropdown menu and the slider, the user can visualize the panel (or cross-section) data,
+              therefore visualize trends in time and across units.'),
              plotlyOutput("linegraph1"))
       
       
@@ -60,18 +71,26 @@ ui<-fluidPage(
 # Define server logic
 server <- (function(input, output) {
   
-  output$linegraph1<- renderPlotly({
-    
-    data1<-data %>%
-      filter(country %in% input$countries)
-    
+    years <- reactive({
+      seq(input$yrrange[1], input$yrrange[2], by = 1)
+    })
+  
+    data1 <- reactive({
+
+      data %>%
+        filter(country %in% input$countries
+               & year %in% years())
+      })
+
+    output$linegraph1<- renderPlotly({
+      
     liv_healthy_years<- 
-      ggplot(data1, aes(x=year, y= healthy_years)) +
+      ggplot(data1(), aes(x=year, y= healthy_years)) +
       geom_path(aes(group=country, 
                     color=country,
                     ids=country),
                 size= 0.75)+
-      geom_text(data = data1 %>% filter(year == last(year)),
+      geom_text(data = data1() %>% filter(year == last(year)),
                 aes(label = country, 
                     x = year + 0.5, 
                     y = healthy_years, 
